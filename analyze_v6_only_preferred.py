c061ccc9bb3a561709743d1de1ecacd41968d31c
BPplays <andymalbp@gmail.com>
2024-05-20 08:48:26 -0700

    fixed

--- analyze_v6_only_preferred.py
+++ analyze_v6_only_preferred.py
@@ -80,7 +80,7 @@ def main(pcapfile):
     print("Flapping:", len(flapping))
     print(", ".join(flapping))
     
-    print("failed:", len(failed))
+    print("failed to analyze:", len(failed))
     for mac in failed:
         print("    "+mac)
 
